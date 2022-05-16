808ae69605f8eef9178e9e9581107cbb6deabc57
DJGosnell <DJGosnell@gmail.com>
2022-05-16 11:04:05 -0400

    Skeleton of the FreeList<T> and SmallList<T>implemented

---
+++ src/DtronixCommon/Collections/Trees/FreeList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtronixCommon.Collections.Trees
+{
+    public class FreeList<T>
+    {
+        // TODO: Review this further.
+        [StructLayout(LayoutKind.Explicit)]
+        public struct FreeElement
+        {
+            [FieldOffset(0)] public T element;
+            [FieldOffset(0)] public int next;
+        }
+
+        private SmallList<FreeElement> data;
+        private int first_free;
+
+        public FreeList()
+        {
+            
+        }
+
+        public int insert(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void erase(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int range()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void reserve(int n)
+        {
+            throw new NotImplementedException();
+        }
+        public void swap(SmallList<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ src/DtronixCommon/Collections/Trees/SmallList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtronixCommon.Collections.Trees
+{
+    public class SmallList<T>
+    {
+        private struct ListData
+        {
+            public T[] buf = new T[256];
+            public T[] data;
+            public int num;
+            public int cap;
+        }
+
+        private ListData ld;
+
+        public SmallList()
+        {
+            
+        }
+
+        public int size()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+        public int find_index(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void reserve(int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void pubsh_back(T element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T pop_back()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void swap(SmallList<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] data()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
