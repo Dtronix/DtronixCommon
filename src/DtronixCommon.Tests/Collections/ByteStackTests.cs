46af735d729be2c77028e8310e410191d13a9445
DJGosnell <DJGosnell@gmail.com>
2023-03-09 11:50:57 -0500

    Started work on AllocatedArrayPool.  This work is based upon ConfiguredArrayPool.

---
+++ src/DtronixCommon.Tests/Collections/ByteStackTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using DtronixCommon.Collections;
+using NUnit.Framework;
+
+namespace DtronixCommon.Tests.Collections;
+
+public class ByteStackTests
+{
+ 
+    [Test]
+    public void PushAndPopValue()
+    {
+        var queue = new ByteStack();
+        queue.Push(50);
+        Assert.AreEqual(50, queue.Pop());
+    }
+
+
+    [Test]
+    public void PushAndPopValues()
+    {
+        var queue = new ByteStack();
+        queue.Push(50);
+        queue.Push(20);
+        queue.Push(90);
+
+        Assert.AreEqual(90, queue.Pop());
+        Assert.AreEqual(20, queue.Pop());
+        Assert.AreEqual(50, queue.Pop());
+    }
+
+    [Test]
+    public void PushesAndPopsValues()
+    {
+        var queue = new ByteStack(5);
+        queue.Push(50);
+        Assert.AreEqual(50, queue.Pop());
+
+        queue.Push(20);
+        queue.Push(90);
+        Assert.AreEqual(90, queue.Pop());
+        Assert.AreEqual(20, queue.Pop());
+
+        queue.Push(1);
+        queue.Push(2);
+        queue.Push(3);
+        queue.Push(4);
+        queue.Push(5);
+        Assert.AreEqual(5, queue.Pop());
+        Assert.AreEqual(4, queue.Pop());
+        Assert.AreEqual(3, queue.Pop());
+        Assert.AreEqual(2, queue.Pop());
+        Assert.AreEqual(1, queue.Pop());
+    }
+
+
+    [Test]
+    public void TryPushReturnsFalseWhenFull()
+    {
+        var queue = new ByteStack(1);
+        queue.Push(50);
+        Assert.IsFalse(queue.TryPush(20));
+    }
+
+
+    [Test]
+    public void PushThrowsWhenFull()
+    {
+        var queue = new ByteStack(1);
+        queue.Push(50);
+        Assert.Throws<InvalidOperationException>(() => queue.Push(20));
+    }
+
+    [Test]
+    public void PopReturnsFalseOnEmpty()
+    {
+        var queue = new ByteStack();
+        Assert.IsFalse(queue.TryPop(out var free));
+    }
+
+    [Test]
+    public void PopsOffInitial()
+    {
+        var queue = new ByteStack(new byte[] {1, 24, 73}, 3);
+        Assert.AreEqual(73, queue.Pop());
+        Assert.AreEqual(24, queue.Pop());
+        Assert.AreEqual(1, queue.Pop());
+    }
+}
---
+++ src/DtronixCommon/Collections/ByteStack.cs
@@ -0,0 +1,114 @@
+﻿namespace DtronixCommon.Collections;
+
+/// <summary>
+/// Queue which has a maximum size of 256 values and a range of: 0 &#8804; value &#8804; 256.
+/// Not thread safe.
+/// </summary>
+/// <remarks>
+/// This class is intended to be used within a non-threaded or locked environment to ensure sequential
+/// pushing and popping of the stack.
+/// </remarks>
+#if SET_INTERNAL
+internal
+#else
+public 
+#endif
+class ByteStack
+{
+    /// <summary>
+    /// Index of the last free index.
+    /// </summary>
+    private int _freeIndex;
+
+    /// <summary>
+    /// Array of the items.
+    /// </summary>
+    private readonly byte[] _queueBuffer;
+
+    /// <summary>
+    /// Number of items in the queue.
+    /// </summary>
+    public int Count => _freeIndex + 1;
+
+    /// <summary>
+    /// Initializes the queue with the specified max size.
+    /// </summary>
+    /// <param name="maxSize">Maximum number of entities for this queue. 0 &#8804; maxSize &#8804; 256.</param>
+    public ByteStack(int maxSize = 256)
+        : this(new byte[maxSize], 0)
+    {
+    }
+
+    /// <summary>
+    /// Initializes the queue with the specified max size.
+    /// </summary>
+    /// <param name="contents">Initial contents of the stack. Length must be 0 &#8804; maxSize &#8804; 256.</param>
+    /// <param name="count">Number to set the initial count to.</param>
+    public ByteStack(byte[] contents, int count)
+    {
+        if (contents.Length > 256 || contents.Length < 1)
+            throw new IndexOutOfRangeException("Max size must be greater than or equal to 1 and less than or equal to 256");
+
+        if (count > contents.Length || count < 0)
+            throw new IndexOutOfRangeException("Count must be greater than zero and less than or equal to the contents length.");
+
+        _freeIndex = count - 1;
+        _queueBuffer = contents;
+    }
+
+    /// <summary>
+    /// Tries to pop the last item off the end of the queue.
+    /// </summary>
+    /// <param name="value">Value popped off the end of the queue</param>
+    /// <returns>True on successful pop. False if the queue is empty.</returns>
+    public bool TryPop(out byte value)
+    {
+        if (_freeIndex == -1)
+        {
+            value = 0;
+            return false;
+        }
+
+        value = _queueBuffer[_freeIndex--];
+        return true;
+    }
+
+    /// <summary>
+    /// Pops the last item off the end of the queue.
+    /// </summary>
+    /// <returns>Popped value.</returns>
+    /// <exception cref="Exception">Throws when queue is empty.</exception>
+    public byte Pop()
+    {
+        if (TryPop(out var value))
+            return value;
+
+        throw new Exception("Queue is empty.");
+    }
+
+    /// <summary>
+    /// Pushes a value onto the queue.
+    /// </summary>
+    /// <param name="value">Value to push.</param>
+    /// <exception cref="InvalidOperationException">If </exception>
+    public void Push(byte value)
+    {
+        if (!TryPush(value))
+            throw new InvalidOperationException(
+                "Tried to return index when the buffer is full.");
+    }
+
+    /// <summary>
+    /// Tries to push a value onto the queue.
+    /// </summary>
+    /// <param name="value">Value to push</param>
+    /// <returns>True on successful push.  False if the queue is full.</returns>
+    public bool TryPush(byte value)
+    {
+        if (_freeIndex >= _queueBuffer.Length - 1)
+            return false;
+
+        _queueBuffer[++_freeIndex] = value;
+        return true;
+    }
+}
